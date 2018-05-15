20c477e4c29e11ba260a05c16ab55f1c43e271ba
김기성 <kkimkisung@gmail.com>
2018-05-15 23:48:49 +0900

    table 3건 추가 - CRM - Breed - Process

--- feature/study_library/django/feedbap/models.py
+++ feature/study_library/django/feedbap/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
 class User(models.Model):
    serialNum = models.CharField(max_length=30, primary_key=True)
    userName = models.CharField(max_length=30, null=False)
@@ -9,3 +11,43 @@ class User(models.Model):
    userPW = models.CharField(max_length=30, null=False)
    phonenumber = models.CharField(max_length=11, null=False, unique=True)
 
+
+# User -> mysql변경시 ...
+# serialNum : (Field) INTEGER -> VARCHAR(30)
+
+
+class CRM(models.Model):
+    serialNum = models.ForeignKey(User, on_delete=models.CASCADE)
+    questDate = models.DateTimeField(auto_now=True)
+    questType = models.CharField(max_length=30, null=False)
+    questTypeFreq = models.IntegerField(null=True)
+    emailNeed = models.CharField(max_length=1, default='F')
+
+# User -> mysql변경시 ...
+# questNum : (Value Name) questNum -> id로 변경되었습니다(기본 django default제공)
+# serialNum : (Field) INTEGER -> VARCHAR(30)
+# emailNeed : (Field) BIT -> VARCHAR(1), (Default) 0 -> 'F'
+
+
+class Process(models.Model):
+    videoNum = models.IntegerField(primary_key=True)
+    multiType = models.CharField(max_length=30)
+    videoType = models.CharField(max_length=30)
+    popular = models.IntegerField(default=0)
+    placeName = models.CharField(max_length=30)
+
+
+class Breed(models.Model):
+    breedName = models.CharField(max_length=30, primary_key=True)
+    maxWeight = models.FloatField(null=False)
+    minWeight = models.FloatField(null=False)
+    lifespan = models.IntegerField(null=False)
+    size = models.FloatField(null=False)
+
+
+
+# class Pet(models.Model):
+#     petId = models.IntegerField(primary_key=True)
+#     serialNum = models.ForeignKey(User, on_delete=models.CASCADE)
+#     petName = models.CharField(max_length=30, null=False)
+#     breedName = models.CharField(max_length=30)
